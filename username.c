4a58aa798e19d3a106684d28bbbe3a420b1205cf
cheolsoonP <mn5724@naver.com>
2017-05-25 17:06:32 +0900

    nothing

--- username.c
+++ username.c
@@ -9,7 +9,7 @@ int inputname(void)
 
   printf("사용자 명을 입력하시오(최대10자): ");
   for(nm=0;nm<10;nm++)
-  scanf("%c", &name[nm]);
+  scanf("%c", &name[nm]); //입력받은 값을 name배열값에 지정.
 
   printf("username: %c%c%c%c%c%c%c%c%c", name[0], name[1], name[2], name[3], name[4], name[5], name[6], name[7], name[8], name[9]);
 
