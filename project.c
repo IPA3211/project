b5b90d73cfdb958138bdb676bb6af4f2d9fbd928
IPA3211 <kygsm2003@naver.com>
2017-05-25 15:45:41 +0900

    read map

--- project.c
+++ project.c
@@ -18,8 +18,50 @@
 
 #include<stdio.h>
 
-//Seung-mo
+//common
+FILE  *map
 
+int map[5][30][30];
+//Seung-mo
+int readmap(void){
+	
+	int a,b = 0, i = 0, j = 0;
+
+	map = fopen("map", "r");
+
+	while (fscanf(map, "%c", &a) != 'e')
+	{	
+		if(a == ' '){
+			map[b][i][j] = 1;
+			j++;
+		}
+		else if(a == '#'){
+			map[b][i][j] = 2;
+			j++;
+		}
+		else if(a == '$'){
+			map[b][i][j] = 3;
+			j++;
+		}
+		else if(a =='O'){
+			map[b][i][j] = 4;
+			j++;
+		}
+		else if(a =='@'){
+			map[b][i][j] = 5;
+			j++;
+		}
+		else if(a =='\n'){
+			i++;
+		}
+		else if(a == 'm'){
+			b++;
+		}
+		else{}		
+	}
+
+
+}
 //Jae-hyun
 
 //Cheol-soon
@@ -30,5 +72,14 @@
 
 int main(void)
 {
-  
+	int a;
+	scanf("%d" &a);
+	
+	readmap();
+
+	for (int i =0; i < 30; i++){
+		for (int j = 0; j < 30; j++)
+			printf("%d" map[a][i][j]);
+		printf("\n")
+	}
 }
