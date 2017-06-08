3ceebd59caef2556fd04cea5763d598ee6d1c2ac
IPA3211 <kygsm2003@naver.com>
2017-06-08 14:00:11 +0900

    asd

--- project.c
+++ project.c
@@ -1,10 +1,10 @@
 /*********************
-* date:
-* group: handsome
-* student ID: 20170259,20170319,20170293,
-*
-*
-********************/
+ * date:
+ * group: handsome
+ * student ID: 20170259,20170319,20170293,20170289
+ *
+ *
+ ********************/
 
 /********************
   message or todo
@@ -15,7 +15,7 @@
   1. read map: clear(on main)
   2. error: clear(on main)
   3. input name: clear(on main)
-  4. game clear: jae-woo
+  4. game clear: clear(on main)
   5. ranking: jae-hyun
   6. save ranking: jae-woo
   7. h,j,k,l move: clear(on main)
@@ -23,13 +23,13 @@
   9. new: clear(on main)
   10. exit: jae-woo
   11. replay: jae-hyun
-  12. file load: jae-woo
-  13. save: jae-woo
+  12. file load: clear(on main)
+  13. save: clear(on main)
   14. help: clear(on main)
   15. top: cheol-soon
   16. each map top: cheol-soon
   17. time: jae-woo
-*********************/
+ *********************/
 
 /* FOR WINDOWS */
 //#include <termio.h>
@@ -39,18 +39,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 /* FOR ERROR FIX */
 int ctrl_key(char ch);
 //common var
 
-int map[5][30][30];
-int p_map[30][30];
-int mapsize[5];
+char map[5][30][30];
+char p_map[30][30];
+int  mapsize[5];
 
 int stage = 0;
 
 char name[10];
+clock_t start,end;
+double m_time,alltime,savetime;
 //common func
 int getch(void){
 	int ch;
@@ -85,7 +87,7 @@ int readmap(void){
 	for(int x = 0; x <5 ; x++)
 		for(int y = 0; y< 30; y++)
 			for(int z = 0; z < 30; z++)
-				map[x][y][z] = 0;
+				map[x][y][z] = ' ';
 
 	while (fscanf(mapf, "%c", &a) != EOF)
 	{
@@ -97,7 +99,7 @@ int readmap(void){
 			i++;
 			j = 0;
 		}
-		else if (a == 97||a =='e'){
+		else if (a == 'm'||a =='e'){
 			mapsize[b] = ++i;
 			b++;
 			i =-1;
@@ -157,8 +159,10 @@ int command(char x){
 			exit(1);
 			break;
 		case 's':
+			save();
 			break;
 		case 'f':
+			fileload();
 			break;
 		case 't':
 			break;
@@ -190,12 +194,13 @@ int get_key(void)
 }
 
 int printname(void){
-	
+
 	printf("\tHello ");
 	for(int i = 0; i < 10; i++)
 		printf("%c", name[i]);
 	printf("\n");
 }
+
 
 //Cheol-soon
 int inputname(void)
@@ -241,142 +246,357 @@ int newgame(void);
 
 //Jae-woo
 int error(void){
-    int box=0, place=0;
-    for(int b=0; b<5; b++){
-        for(int i=0; i<30; i++)
-            for(int j=0; j<30; j++)
-            {
-                if(map[b][i][j]=='$')
-                    box++;
-                else if(map[b][i][j]=='O')
-                    place++;
-                else{}
-            }
-        if(box!=place){
-            printf("%d맵에 오류가 있습니다.\n", b);
-            exit(1);
-        }
-    }
+	int box=0, place=0;
+	for(int b=0; b<5; b++){
+		for(int i=0; i<30; i++)
+			for(int j=0; j<30; j++)
+			{
+				if(map[b][i][j]=='$')
+					box++;
+				else if(map[b][i][j]=='O')
+					place++;
+				else{}
+			}
+		if(box!=place){
+			printf("%d맵에 오류가 있습니다.\n", b);
+			exit(1);
+		}
+	}
 }
+int gameclear(void)
+	//game클리어 했을 때
+{
+	if(stage> 4){
+	system("clear");
+	printf("축하합니다. 모든 게임을 완료했습니다.\n");
+	exit(1);
+	}
+}
+int fileload(void)
+{ //f키를 눌렀을 때
+	FILE *sokoban;
+	sokoban=fopen("sokoban.txt","r");
+	for(int i=0;i<30;i++)
+		for(int j=0;j<30;j++)
+			fscanf(sokoban,"%c",&p_map[i][j]);
+	fscanf(sokoban,"%3f",&savetime);
+	fscanf(sokoban,"%d",&stage);
+	fclose(sokoban);
+	start=clock();
+}
+int save(void)
+{ //s키
+	end=clock();
+	savetime= (end-start)/1000;
+	FILE *sokoban;
+	sokoban=fopen("sokoban.txt","w");
+	for(int i=0; i<30;i++)
+		for(int j=0; j<30; j++)
+		{
+			fprintf(sokoban,"%c",p_map[i][j]);
+		}
+	fprintf(sokoban,"%3f",savetime);
+	fprintf(sokoban,"%d",stage);
+	fclose(sokoban);
+	start=clock();
+}
+/*int timeprint(void){
+  if(//fileload했을때)
+  m_time=savetime+(end-start)/1000;
+  else
+  m_time= (end-start)/1000;
+  alltime+=time;
+  printf("클리어 시간: %3f\n",m_time);
+  }*/
 //Jae-hyun
 int ctrl_key(char ch)
 {
-	int a, b;
-  for(int i=0;i<30;i++)
-  {
-    for(int j=0;j<30;j++)
+    int a, b, brilliant;
+    for(int i=0;i<30;i++)
     {
-      if ((p_map[i][j] == '@')||(p_map[i][j] == '*')){
-        b = i, a = j;
-      break;
-	  }
+        for(int j=0;j<30;j++)
+        {
+            if ((p_map[i][j] == '@')||(p_map[i][j] == '*')){
+                b = i, a = j;
+                break;
+            }
+        }
     }
-   }
-
-    if((ch == 'l')&&((p_map[b][a+1] == ' ')||p_map[b][a+1] == 'O'))//right
+    //right
+    
+    if((ch == 'l')&&(p_map[b][a+1] == ' '))//player heading to space
     {
-      p_map[b][a] = p_map[b][a+1];
-      p_map[b][a+1] = '@';
-      a += 1;
+        if(brilliant == 0)
+        {
+            p_map[b][a] = p_map[b][a+1];
+            p_map[b][a+1] = '@';
+            a += 1;
+        }
+        else if(brilliant == 1)
+        {
+            p_map[b][a] = 'O';
+            p_map[b][a+1] = '@';
+            a += 1;
+            brilliant = 0;
+        }
     }
-    else if((ch == 'l')&&(p_map[b][a+1]) == '#')
+    else if((ch == 'l')&&(p_map[b][a+1]) == '#')//player confronting wall
+    {}
+    else if((ch == 'l')&&(p_map[b][a+1]) == '$')//player confronting package
     {
-
+        
+        if(p_map[b][a+2] == '#')//package confronting the wall
+        {}
+        else if(p_map[b][a+2] == ' ')//package heading to space
+        {
+            p_map[b][a] = p_map[b][a+2];
+            p_map[b][a+1] = '@';
+            p_map[b][a+2] = '$';
+            a += 1;
+        }
     }
-    else if((ch == 'l')&&(p_map[b][a+1]) == '$')
+    else if((ch == 'l')&&(p_map[b][a+1] == 'O'))
     {
-      if(p_map[b][a+2] == '#')
-      {}
-      else if(p_map[b][a+2] == ' ')
-      {
-        p_map[b][a] = p_map[b][a+2];
-        p_map[b][a+1] = '@';
+        p_map[b][a+1] = p_map[b][a];
+        p_map[b][a] = ' ';
+        brilliant = 1;//player on the location
+        a += 1;
+        if(brilliant == 1)
+        {
+            p_map[b][a] = 'O';
+            p_map[b][a+1] = '@';
+            a += 1;
+            brilliant = 0;
+        }
+    }
+    if((ch == 'l')&&(p_map[b][a+1] == '$')&&(p_map[b][a+2] == 'O'))
+    {
+        p_map[b][a+1] = p_map[b][a];
+        p_map[b][a+2] = '*';
+        p_map[b][a] = ' ';
+        a += 1;
+    }
+    else if((ch == 'l')&&(p_map[b][a+1] == '*')&&(p_map[b][a+2] == 'O'))
+    {
+        p_map[b][a] = 'O';
+        p_map[b][a+1] = p_map[b][a+2];
+        p_map[b][a+2] = '*';
+        a += 1;
+        brilliant = 1;
+    }
+    else if((ch == 'l')&&(p_map[b][a+1] == '*')&&(p_map[b][a+2] == ' '))
+    {
+        p_map[b][a+1] = p_map[b][a];
+        p_map[b][a] = 'O';
         p_map[b][a+2] = '$';
-				a += 1;
-      }
+        a += 1;
     }
-		else if((ch == 'h')&&(p_map[b][a-1]) == ' ')//left
-		{
-			p_map[b][a] = p_map[b][a-1];
-			p_map[b][a-1] = '@';
-			a -= 1;
-		}
-		else if((ch == 'h')&&(p_map[b][a-1]) == '#')
-		{
-
-		}
-		else if((ch == 'h')&&(p_map[b][a-1]) == '$')
-		{
-			if(p_map[b][a-2] == '#')
-			{}
-			else if(p_map[b][a-2] == ' ')
-			{
-				p_map[b][a] = p_map[b][a-2];
-				p_map[b][a-1] = '@';
-				p_map[b][a-2] = '$';
-				a -= 1;
-			}
-		}
-		else if((ch == 'k')&&(p_map[b+1][a]) == ' ')//up
-		{
-			p_map[b][a] = p_map[b+1][a];
-			p_map[b+1][a] = '@';
-			b += 1;
-		}
-		else if((ch == 'k')&&(p_map[b+1][a]) == '#')
-		{
-
-		}
-		else if((ch == 'k')&&(p_map[b+1][a]) == '$')
-		{
-			if(p_map[b+2][a] == '#')
-			{}
-			else if(p_map[b+2][a] == ' ')
-			{
-				p_map[b][a] = p_map[b+2][a];
-				p_map[b+1][a] = '@';
-				p_map[b+2][a] = '$';
-				b += 1;
-			}
-		}
-		else if((ch == 'j')&&(p_map[b-1][a]) == ' ')//down
-		{
-			p_map[b][a] = p_map[b-1][a];
-			p_map[b-1][a] = '@';
-			b -= 1;
-		}
-		else if((ch == 'j')&&(p_map[b-1][a]) == '#')
-		{
-
-		}
-		else if((ch == 'j')&&(p_map[b-1][a]) == '$')
-		{
-			if(p_map[b-2][a] == '#')
-			{}
-			else if(p_map[b-2][a] == ' ')
-			{
-				p_map[b][a] = p_map[b-2][a];
-				p_map[b-1][a] = '@';
-				p_map[b-2][a] = '$';
-				b -= 1;
-			}
-		}
-		return 0;
+    if((ch == 'l')&&((p_map[b][a+1] == '$')||(p_map[b][a+1] == '*'))&&((p_map[b][a+2] == '$')||(p_map[b][a+2] == '*')))
+    {}
+    
+    //left
+    if((ch == 'h')&&((p_map[b][a-1] == ' ')||p_map[b][a-1] == 'O'))//player heading to space
+    {
+        if(brilliant == 0)
+        {
+            p_map[b][a] = p_map[b][a-1];
+            p_map[b][a-1] = '@';
+            a -= 1;
+        }
+        else if(brilliant == 1)
+        {
+            p_map[b][a] = 'O';
+            p_map[b][a+1] = '@';
+            a -= 1;
+            brilliant = 0;
+        }
+    }
+    else if((ch == 'h')&&(p_map[b][a-1]) == '#')//player confronting wall
+    {}
+    else if((ch == 'h')&&(p_map[b][a-1]) == '$')//player confronting package
+    {
+        
+        if(p_map[b][a-2] == '#')//package confronting the wall
+        {}
+        else if(p_map[b][a-2] == ' ')//package heading to space
+        {
+            p_map[b][a] = p_map[b][a-2];
+            p_map[b][a-1] = '@';
+            p_map[b][a-2] = '$';
+            a -= 1;
+        }
+    }
+    else if((ch == 'h')&&(p_map[b][a-1] == 'O'))
+    {
+        p_map[b][a-1] = p_map[b][a];
+        p_map[b][a] = ' ';
+        brilliant = 1;//player on the location
+        a -= 1;
+    }
+    if((ch == 'h')&&(p_map[b][a-1] == '$')&&(p_map[b][a-2] == 'O'))
+    {
+        p_map[b][a-1] = p_map[b][a];
+        p_map[b][a-2] = '*';
+        p_map[b][a] = ' ';
+        a -= 1;
+    }
+    else if((ch == 'h')&&(p_map[b][a-1] == '*')&&(p_map[b][a-2] == 'O'))
+    {
+        p_map[b][a] = 'O';
+        p_map[b][a-1] = p_map[b][a-2];
+        p_map[b][a-2] = '*';
+        a -= 1;
+        brilliant = 1;
+    }
+    else if((ch == 'h')&&(p_map[b][a-1] == '*')&&(p_map[b][a-2] == ' '))
+    {
+        p_map[b][a-1] = p_map[b][a];
+        p_map[b][a] = 'O';
+        p_map[b][a-2] = '$';
+        a -= 1;
+    }
+    if((ch == 'h')&&((p_map[b][a-1] == '$')||(p_map[b][a-1] == '*'))&&((p_map[b][a-2] == '$')||(p_map[b][a-2] == '*')))
+    {}
+    //up
+    if((ch == 'k')&&((p_map[b+1][a] == ' ')||p_map[b+1][a] == 'O'))//player heading to space
+    {
+        if(brilliant == 0)
+        {
+            p_map[b][a] = p_map[b+1][a];
+            p_map[b+1][a] = '@';
+            b += 1;
+        }
+        else if(brilliant == 1)
+        {
+            p_map[b][a] = 'O';
+            p_map[b+1][a] = '@';
+            b += 1;
+            brilliant = 0;
+        }
+    }
+    else if((ch == 'k')&&(p_map[b+1][a]) == '#')//player confronting wall
+    {}
+    else if((ch == 'k')&&(p_map[b+1][a]) == '$')//player confronting package
+    {
+        
+        if(p_map[b+2][a] == '#')//package confronting the wall
+        {}
+        else if(p_map[b+2][a] == ' ')//package heading to space
+        {
+            p_map[b][a] = p_map[b+2][a];
+            p_map[b+1][a] = '@';
+            p_map[b+2][a] = '$';
+            b += 1;
+        }
+    }
+    else if((ch == 'k')&&(p_map[b+1][a] == 'O'))
+    {
+        p_map[b+1][a] = p_map[b][a];
+        p_map[b][a] = ' ';
+        brilliant = 1;//player on the location
+        b += 1;
+    }
+    if((ch == 'k')&&(p_map[b+1][a] == '$')&&(p_map[b+2][a] == 'O'))
+    {
+        p_map[b+1][a] = p_map[b][a];
+        p_map[b+2][a] = '*';
+        p_map[b][a] = ' ';
+        b += 1;
+    }
+    else if((ch == 'k')&&(p_map[b+1][a] == '*')&&(p_map[b+2][a] == 'O'))
+    {
+        p_map[b][a] = 'O';
+        p_map[b+1][a] = p_map[b+2][a];
+        p_map[b+2][a] = '*';
+        b += 1;
+        brilliant = 1;
+    }
+    else if((ch == 'k')&&(p_map[b+1][a] == '*')&&(p_map[b+2][a] == ' '))
+    {
+        p_map[b+1][a] = p_map[b][a];
+        p_map[b][a] = 'O';
+        p_map[b+2][a] = '$';
+        b += 1;
+    }
+    if((ch == 'k')&&((p_map[b+1][a] == '$')||(p_map[b+1][a] == '*'))&&((p_map[b+2][a] == '$')||(p_map[b+2][a] == '*')))
+    {}
+    //down
+    if((ch == 'j')&&((p_map[b-1][a] == ' ')||p_map[b-1][a] == 'O'))//player heading to space
+    {
+        if(brilliant == 0)
+        {
+            p_map[b][a] = p_map[b-1][a];
+            p_map[b-1][a] = '@';
+            b -= 1;
+        }
+        else if(brilliant == 1)
+        {
+            p_map[b][a] = 'O';
+            p_map[b-1][a] = '@';
+            b -= 1;
+            brilliant = 0;
+        }
+    }
+    else if((ch == 'j')&&(p_map[b-1][a]) == '#')//player confronting wall
+    {}
+    else if((ch == 'j')&&(p_map[b-1][a]) == '$')//player confronting package
+    {
+        
+        if(p_map[b-2][a] == '#')//package confronting the wall
+        {}
+        else if(p_map[b-2][a] == ' ')//package heading to space
+        {
+            p_map[b][a] = p_map[b-2][a];
+            p_map[b-1][a] = '@';
+            p_map[b-2][a] = '$';
+            b -= 1;
+        }
+    }
+    else if((ch == 'j')&&(p_map[b-1][a] == 'O'))
+    {
+        p_map[b-1][a] = p_map[b][a];
+        p_map[b][a] = ' ';
+        brilliant = 1;//player on the location
+        b -= 1;
+    }
+    if((ch == 'j')&&(p_map[b-1][a] == '$')&&(p_map[b-2][a] == 'O'))
+    {
+        p_map[b-1][a] = p_map[b][a];
+        p_map[b-2][a] = '*';
+        p_map[b][a] = ' ';
+        b -= 1;
+    }
+    else if((ch == 'j')&&(p_map[b-1][a] == '*')&&(p_map[b-2][a] == 'O'))
+    {
+        p_map[b][a] = 'O';
+        p_map[b-1][a] = p_map[b-2][a];
+        p_map[b-2][a] = '*';
+        b -= 1;
+        brilliant = 1;
+    }
+    else if((ch == 'j')&&(p_map[b-1][a] == '*')&&(p_map[b-2][a] == ' '))
+    {
+        p_map[b-1][a] = p_map[b][a];
+        p_map[b][a] = 'O';
+        p_map[b-2][a] = '$';
+        b -= 1;
+    }
+    if((ch == 'j')&&((p_map[b-1][a] == '$')||(p_map[b-1][a] == '*'))&&((p_map[b-2][a] == '$')||(p_map[b-2][a] == '*')))
+    {}//main
 }
-//main
-
-int main(void)
-{
-	system("clear");
-	readmap();
-	error();
-	playmap(stage);
-	inputname();
-	system("clear");
-	while(1){
-		printname();
-		showgame();
-		get_key();
-		system("clear");
-	}
-}
+    int main(void)
+    {
+        system("clear");
+        readmap();
+        error();
+        playmap(stage);
+        inputname();
+        system("clear");
+        while(1){
+            printname();
+            showgame();
+            get_key();
+            gameclear();
+            system("clear");
+        }
+    }
